9860571c7a510e9cf3ce72589797a03e69ab3a59
Jason Swetzoff <swetzoff@gmail.com>
2016-02-08 15:56:47 -0800

    Adding svg's. Adding JS. Working on styles.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log("JavaScript is Compiled");
+smoothScroll.init();
+
+(function(){
+
+// Activate menu items
+
+    $('.js-menuLink').click(function(){
+        var menuLink = $('.js-menuLink');
+        var menuItems = menuLink.parent();
+
+        if ($('menuItmes').hasClass('is-current')) {
+            return false;
+        }
+
+        menuItems.removeClass('is-current');
+        $(this).parent().addClass('is-current');
+    });
+
+})(window);
