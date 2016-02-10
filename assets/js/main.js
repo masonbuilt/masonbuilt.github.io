9e8f232d6dc2908250251caf18506f5a2f8bb46c
Jason Swetzoff <swetzoff@gmail.com>
2016-02-10 14:12:18 -0800

    Working on global positioning and spacing.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,20 @@
 smoothScroll.init();
+gumshoe.init({
+    // selector: '[data-gumshoe] a' // Default link selector (must use a valid CSS selector)
+    // selectorHeader: '[data-gumshoe-header]' // Fixed header selector (must use a valid CSS selector)
+    offset: 5, // Distance in pixels to offset calculations
+    activeClass: 'is-current', // Class to apply to active navigation link and it's parent list item
+    // callback: function (nav) {} // Callback to run after setting active link
+});
 
 (function(){
 
-// Activate menu items
+// Contact us
 
-    $('.js-menuLink').click(function(){
-        var menuLink = $('.js-menuLink');
-        var menuItems = menuLink.parent();
-
-        if ($('menuItmes').hasClass('is-current')) {
-            return false;
-        }
-
-        menuItems.removeClass('is-current');
-        $(this).parent().addClass('is-current');
-    });
+        $('.js-btnContact').click(function() {
+            event.preventDefault();
+            $('.js-contactWrap').toggleClass('is-active');
+            $(this).toggleClass('is-down');
+        })
 
 })(window);
